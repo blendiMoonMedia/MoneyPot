5c3ec6cb4d27fbb5137fa74cf612d62123631877
Blendi Misini <blendimisini@gmail.com>
2023-03-15 16:44:57 +0100

    MP: Added casino tags

---
+++ src/api/casino-tag/controllers/casino-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * casino-tag controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::casino-tag.casino-tag');
---
+++ src/api/casino-tag/routes/casino-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * casino-tag router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::casino-tag.casino-tag');
